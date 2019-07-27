04e12c6f48bbaa164548486e3579281aff14c45a
wrcj12138aaa <wrcj12138@163.com>
2019-07-27 22:35:58 +0800

    Initial commit

---
+++ src/main/java/com/one/learn/springboot/actions/profiles/SpringbootActionsProfilesApplication.java
@@ -0,0 +1,16 @@
+package com.one.learn.springboot.actions.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author One
+ */
+@SpringBootApplication
+public class SpringbootActionsProfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActionsProfilesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/one/learn/springboot/actions/profiles/controller/ProfileController.java
@@ -0,0 +1,13 @@
+package com.one.learn.springboot.actions.profiles.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试 Controller
+ *
+ * @author One
+ * @date 2019/07/27
+ */
+@RestController()
+public class ProfileController {
+}
